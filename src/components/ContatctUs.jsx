7201f77b7a76d146a17a4ba294eddc9987f707bb
ShabiGardezi <shabigardezi51214@gmail.com>
2023-08-09 12:23:39 -0500

    Contact Us CSS

--- src/components/ContatctUs.jsx
+++ src/components/ContatctUs.jsx
@@ -12,25 +12,31 @@ const ContatctUs = () => {
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consequat tortor lorem, quis tempor felis commodo vel."
       />
       <div className="Contact-Content">
-        <div className="leftSide">
-          <h2 className="title">Contact Us</h2>
-          <h2 className="heading">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          </h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-            consequat tortor lorem, quis tempor felis commodo vel. Mauris eu
-            vulputate augue. Fusce nisi risus, ornare vitae egestas sit amet,
-            luctus at tortor.
-          </p>
-          <div className="contact-info">
-            <p>(111) 222-3333</p>
-            <p>support@shopsatellitetv.com</p>
-            <p>UNITED STATES</p>
+        <div className="row">
+          <div className="col-6">
+            <div className="leftSide">
+              <h2 className="title">Contact Us</h2>
+              <h2 className="heading">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                consequat tortor lorem, quis tempor felis commodo vel. Mauris eu
+                vulputate augue. Fusce nisi risus, ornare vitae egestas sit
+                amet, luctus at tortor.
+              </p>
+              <div className="contact-info">
+                <p>(111) 222-3333</p>
+                <p>support@shopsatellitetv.com</p>
+                <p>UNITED STATES</p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="rightSide">
-          <ContactForm />
+          <div className="col-6">
+            <div className="rightSide">
+              <ContactForm />
+            </div>
+          </div>
         </div>
       </div>
     </div>
