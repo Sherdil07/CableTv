635133ccc1a4d876d6e2d9211628a3fe6f2d7705
ShabiGardezi <shabigardezi51214@gmail.com>
2023-08-10 01:32:31 +0500

    Services Pages Design

--- src/pages/ProvidersCommon.jsx
+++ src/pages/ProvidersCommon.jsx
@@ -5,13 +5,19 @@ const ProvidersCommon = ({ title, heading, description, imageSrc }) => {
   return (
     <div className="ProvidersCommon">
       <div className="content">
-        <div className="leftSide">
-          <h2 className="title">{title}</h2>
-          <h2 className="heading">{heading}</h2>
-          <p className="description">{description}</p>
-        </div>
-        <div className="rightSide">
-          <img src={imageSrc} alt="" />
+        <div className="row">
+          <div className="col-6">
+            <div className="LeftSide">
+              <img src={imageSrc} alt="" />
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="RightSide">
+              <h2 className="title">{title}</h2>
+              <h2 className="heading">{heading}</h2>
+              <p className="description">{description}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
