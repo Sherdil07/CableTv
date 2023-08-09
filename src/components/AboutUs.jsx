9ffc0521826f7bfe9f968ac00b5971b0ef5c603e
ShabiGardezi <shabigardezi51214@gmail.com>
2023-08-09 12:06:52 -0500

    Navbar Header

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,6 +3,7 @@ import About from "../pages/About";
 import AboutSatteliteTv from "../pages/AboutSatteliteTv";
 import HeroSection from "../pages/HeroSection";
 import ServiceProvider from "../pages/ServiceProviders";
+import "../styles/About.css";
 
 const AboutUs = () => {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-// Navbar.js
-import React, { useState } from "react";
+import React from "react";
 import logo from "../assests/header-Logo.png";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
@@ -12,32 +11,19 @@ const servicesOptions = [
 ];
 
 function Navbar() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
-
   return (
     <div className="container">
-      <div className={`Navbar ${sidebarOpen ? "open-sidebar" : ""}`}>
+      <div className="Navbar">
         <div className="leftSide">
           <img src={logo} alt="logo" />
         </div>
         <div className="rightSide">
-          {/* Toggle Button for Opening/Closing Sidebar */}
-          <button className="sidebar-toggle" onClick={toggleSidebar}>
-            ☰
-          </button>
-        </div>
-        <div className={`sidebar ${sidebarOpen ? "open" : ""}`}>
-          {/* Sidebar Content */}
           <Link to="/"> Home </Link>
           <Link to="/about"> About Us</Link>
           <div className="dropdown">
             <button className="dropbtn">
-              <Link to="/services"> Services</Link>
-            </button>
+            <Link to="/services"> Services</Link>
+              </button>
             <div className="dropdown-content">
               {servicesOptions.map((service, index) => (
                 <Link
@@ -51,11 +37,13 @@ function Navbar() {
           </div>
           <Link to="/serviceproviders"> Service Providers</Link>
           <Link to="/contactus"> Contact Us</Link>
-          <input
-            className="header-searchBar"
-            type="text"
-            placeholder="Enter Zip Code"
-          />
+          <>
+            <input
+              className="header-searchBar"
+              type="text"
+              placeholder="Enter Zip Code"
+            />
+          </>
         </div>
       </div>
     </div>
