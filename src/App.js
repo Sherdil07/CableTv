da1752740689ce9b4a3a246a312d262f29302754
ShabiGardezi <shabigardezi51214@gmail.com>
2023-08-28 12:56:31 -0500

    pages content

--- src/App.js
+++ src/App.js
@@ -15,13 +15,8 @@ import SatelliteTv from "./pages/SatelliteTv";
 import CableTv from "./pages/CableTv";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import ZipCodeInfo from "./components/zipCodeInfo";
-import Dashboard from "./components/Dashboard";
-import SignUp from "./components/SignUp";
-import Login from "./components/Login";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   return (
     <div className="App">
       <Navbar />
@@ -32,7 +27,6 @@ function App() {
         <Route path="/services" exact Component={Services} />
         <Route path="/serviceproviders" exact Component={ProvidersTab} />
         <Route path="/contactus" exact Component={ContatctUs} />
-        <Route path="/write-json" exact Component={Dashboard} />
         <Route path="/privacy-policy" exact Component={PrivacyPolicy} />
         <Route path="services/cable-internet" exact Component={CableInternet} />
         <Route path="services/cable-tv" exact Component={CableTv} />
@@ -44,23 +38,10 @@ function App() {
         <Route path="services/satellite-tv" exact Component={SatelliteTv} />
         <Route path="/privacypolicy" exact Component={PrivacyPolicy} />
         <Route path="/zipCode/:zipCode" exact Component={ZipCodeInfo} />
-        <Route path="/signup" exact Component={SignUp} />
-        <Route
-          path="/login"
-          exact element={<Login setIsLoggedIn={setIsLoggedIn} />} // This route should have the prop
-        />
-        <Route
-          path="/dashboard/*"
-          exact element={
-            isLoggedIn ? <ProtectedDashboard /> : <Navigate to="/login" />
-          } // Protected route
-        />
       </Routes>
       <Footer />
     </div>
   );
 }
-function ProtectedDashboard() {
-  return <Dashboard />;
-}
+
 export default App;
--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -15,17 +15,49 @@ const PrivacyPolicy = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </h2>
         <p>
-          
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac libero varius, euismod felis dapibus, placerat mauris. Aenean sollicitudin turpis quis lectus viverra dignissim. Pellentesque leo magna, rutrum non cursus non, pellentesque vel quam. Nulla tempus velit at tincidunt porttitor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec fermentum mollis orci a commodo. Nam quis magna non lectus interdum iaculis. Integer tortor erat, iaculis quis nisl at, viverra fringilla tortor. Pellentesque ut sem in magna bibendum congue in in sem. Fusce luctus ipsum eu venenatis hendrerit. Integer elementum diam et enim condimentum, vel pharetra diam ornare. Sed a sem ex.
-
-Morbi magna lorem, laoreet in leo a, auctor cursus enim. Morbi ut nunc vestibulum, aliquam sapien sed, finibus lorem. Aenean tincidunt massa dui, non rutrum nibh finibus nec. Nullam viverra pretium aliquam. Morbi quis aliquet tortor. Ut eget interdum enim. Fusce dapibus nulla nunc, quis sagittis sem feugiat sollicitudin. Ut ut nunc sit amet nibh auctor rutrum sed eget mauris. Donec at est eget ligula egestas bibendum sed id odio. Nulla venenatis porttitor metus in congue. Quisque pellentesque, est eu mattis fermentum, sapien leo sodales diam, sed accumsan purus lorem at dolor. Ut eget sagittis sapien. Ut ultricies erat sit amet ligula lobortis auctor.
-
-Proin vel metus eu nisi faucibus pulvinar. Etiam tempor aliquet tristique. Maecenas sit amet accumsan lacus. Nullam rhoncus luctus ornare. Nunc tempor massa a maximus semper. Nullam aliquam sollicitudin mi ut bibendum. Quisque non accumsan elit. Nunc malesuada tincidunt felis, a egestas est faucibus nec. Praesent ornare nisl vel nulla pharetra sodales. Ut volutpat eros nulla, et feugiat metus faucibus sit amet. Nulla cursus magna ac fermentum viverra. Aliquam erat volutpat.
-
-Cras lacinia risus libero, ac facilisis risus eleifend sed. Aliquam eget egestas nulla. Cras facilisis maximus viverra. Donec laoreet dignissim nibh, eget pulvinar quam blandit et. Mauris non placerat eros. Curabitur vulputate convallis eros ac gravida. Nam condimentum ultrices enim. Nulla tincidunt augue nec nisl blandit dignissim ac feugiat elit. Pellentesque gravida, tortor semper faucibus egestas, nisl nisi posuere odio, ac bibendum odio nisl a nisi. Nam lorem metus, sagittis id egestas in, dictum tincidunt neque.
-
-Nullam eleifend enim a sapien ultricies mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec eu vestibulum tortor. Integer eget enim id ante vestibulum sagittis. Praesent mollis neque nec lobortis scelerisque. Donec id sagittis tellus, sit amet varius ligula. Proin lacinia tristique metus non fermentum. Aliquam ac quam tortor. Nulla interdum, mauris sed euismod mollis, lorem est varius orci, eu dignissim tortor nisi eu neque. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac
+          libero varius, euismod felis dapibus, placerat mauris. Aenean
+          sollicitudin turpis quis lectus viverra dignissim. Pellentesque leo
+          magna, rutrum non cursus non, pellentesque vel quam. Nulla tempus
+          velit at tincidunt porttitor. Vestibulum ante ipsum primis in faucibus
+          orci luctus et ultrices posuere cubilia curae; Donec fermentum mollis
+          orci a commodo. Nam quis magna non lectus interdum iaculis. Integer
+          tortor erat, iaculis quis nisl at, viverra fringilla tortor.
+          Pellentesque ut sem in magna bibendum congue in in sem. Fusce luctus
+          ipsum eu venenatis hendrerit. Integer elementum diam et enim
+          condimentum, vel pharetra diam ornare. Sed a sem ex. Morbi magna
+          lorem, laoreet in leo a, auctor cursus enim. Morbi ut nunc vestibulum,
+          aliquam sapien sed, finibus lorem. Aenean tincidunt massa dui, non
+          rutrum nibh finibus nec. Nullam viverra pretium aliquam. Morbi quis
+          aliquet tortor. Ut eget interdum enim. Fusce dapibus nulla nunc, quis
+          sagittis sem feugiat sollicitudin. Ut ut nunc sit amet nibh auctor
+          rutrum sed eget mauris. Donec at est eget ligula egestas bibendum sed
+          id odio. Nulla venenatis porttitor metus in congue. Quisque
+          pellentesque, est eu mattis fermentum, sapien leo sodales diam, sed
+          accumsan purus lorem at dolor. Ut eget sagittis sapien. Ut ultricies
+          erat sit amet ligula lobortis auctor. Proin vel metus eu nisi faucibus
+          pulvinar. Etiam tempor aliquet tristique. Maecenas sit amet accumsan
+          lacus. Nullam rhoncus luctus ornare. Nunc tempor massa a maximus
+          semper. Nullam aliquam sollicitudin mi ut bibendum. Quisque non
+          accumsan elit. Nunc malesuada tincidunt felis, a egestas est faucibus
+          nec. Praesent ornare nisl vel nulla pharetra sodales. Ut volutpat eros
+          nulla, et feugiat metus faucibus sit amet. Nulla cursus magna ac
+          fermentum viverra. Aliquam erat volutpat. Cras lacinia risus libero,
+          ac facilisis risus eleifend sed. Aliquam eget egestas nulla. Cras
+          facilisis maximus viverra. Donec laoreet dignissim nibh, eget pulvinar
+          quam blandit et. Mauris non placerat eros. Curabitur vulputate
+          convallis eros ac gravida. Nam condimentum ultrices enim. Nulla
+          tincidunt augue nec nisl blandit dignissim ac feugiat elit.
+          Pellentesque gravida, tortor semper faucibus egestas, nisl nisi
+          posuere odio, ac bibendum odio nisl a nisi. Nam lorem metus, sagittis
+          id egestas in, dictum tincidunt neque. Nullam eleifend enim a sapien
+          ultricies mattis. Pellentesque habitant morbi tristique senectus et
+          netus et malesuada fames ac turpis egestas. Donec eu vestibulum
+          tortor. Integer eget enim id ante vestibulum sagittis. Praesent mollis
+          neque nec lobortis scelerisque. Donec id sagittis tellus, sit amet
+          varius ligula. Proin lacinia tristique metus non fermentum. Aliquam ac
+          quam tortor. Nulla interdum, mauris sed euismod mollis, lorem est
+          varius orci, eu dignissim tortor nisi eu neque.
         </p>
       </div>
     </div>
